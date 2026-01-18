e85a5db039f560800825ee37a585397c1a438447
wissem012-code <medsaadmed@gmail.com>
2026-01-18 02:48:15 +0100

    fix: add vite-plugin-top-level-await to resolve Prisma WASM warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig, type UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the Vite server.
@@ -49,6 +50,7 @@ export default defineConfig({
     },
   },
   plugins: [
+    topLevelAwait(),
     reactRouter(),
     tsconfigPaths(),
   ],
